a034e754e0d012f0fd9e6237906d6189eb5c3deb
arina-korkhova <askorkhova@gmail.com>
2023-01-02 23:00:57 +0200

    Added files

---
+++ Sportsclub-SQL-Project.sql
@@ -0,0 +1,155 @@
+DROP DATABASE IF EXISTS sportsclub;
+CREATE DATABASE IF NOT EXISTS sportsclub;
+USE sportsclub;
+
+-- creating tables 
+
+CREATE TABLE IF NOT EXISTS Trainers
+(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+	flName 		VARCHAR(40) NOT NULL,
+	category 	CHAR(1),
+					CHECK (category = 'A' OR category = 'B' OR category = 'C' OR category = 'D' OR category = 'E' OR NULL),
+	rate 		INT
+);
+
+CREATE TABLE IF NOT EXISTS Sportsmen
+(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+	flName 		VARCHAR(40) NOT NULL,
+	birthDate 	DATE NOT NULL, 
+	sex 		CHAR(1) NOT NULL DEFAULT 'm',
+					CHECK (sex = 'm' OR sex = 'f'),
+	category 	CHAR(1),
+					CHECK (category = 'A' OR category = 'B' OR category = 'C' OR category = 'D' OR category = 'E' OR NULL),
+	trainer 	INT NOT NULL,	
+	rate 		INT,
+	scholarship INT DEFAULT 0, 
+	address 	VARCHAR(40) NOT NULL,
+	mobile 		VARCHAR(13) NULL,  
+	homephone 	VARCHAR(7) NULL,
+	UNIQUE (mobile, homephone), 
+    CONSTRAINT sportsmen_trainer_fk
+    FOREIGN KEY (trainer) REFERENCES Trainers(id)
+);
+
+CREATE TABLE IF NOT EXISTS Competition
+(
+	id 		 INT AUTO_INCREMENT PRIMARY KEY,
+	comptype VARCHAR(40), 
+	location VARCHAR(40),
+	compdate DATE,
+	agelimit INT 
+);
+
+CREATE TABLE IF NOT EXISTS Participation
+(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+	competition INT,	
+	sportsman 	INT,	
+	result 		INT,
+	place 		INT,
+	FOREIGN KEY (sportsman) REFERENCES Sportsmen(id),
+	FOREIGN KEY (competition) REFERENCES Competition(id)	
+);
+
+-- Fill in the database tables using the INSERT command.
+
+INSERT INTO Trainers (flName, category, rate) VALUES 
+	('Violet Elle Woods', 'B', 2100), 
+	('Max Dean Bailey', 'A', 2300),
+	('Lola Jane Keith', 'A', 2350),
+	('Savannah Marie Mathews', 'A', 2399), 
+	('Lena Kiana Maddox', 'B', 2050),
+	('Luke James Hooper', 'B', 2000);
+
+INSERT INTO Sportsmen (flName, birthDate, sex, category, trainer, rate, scholarship, address, mobile, homephone) VALUES
+	('Dominick Tom Jameson', '1996-10-22', 'm', 'D', 1, 1700, 1500, '3125 Kessla Way', '+380661558796', '7485139'),
+	('Steven Jack Ball', '1997-05-13', 'm', 'C', 2, 1900, 1600, '68878 Fadel Route', '+380957892548', '7190077'),
+	('Ross Max Booth', '1998-03-02', 'm', 'E', 5, 1500, 0, '992 Lempi Creek', NULL, NULL),
+	('Richard Frank Jones', '1989-01-07', 'm', 'D', 4, 1600, 0, '33 Crestview Road', '+380509801433', '7095240'),
+	('Kathleen Anne Hamilton', '1999-05-29', 'f', 'E', 6, 1550, 1400, '31 Holly Road', '+380675207119', NULL),
+	('Susan Jane Morris', '2000-10-31', 'm', 'D', 6, 1650, 1800, '28 Settlement Avenue', NULL, '7196483'),
+	('Kimberly Maria Perez', '1998-09-07', 'f', 'C', 3, 1800, 1900, '32 Willamette Avenue', '+380507890049', '7198825'),
+	('Melissa Kira Pittman', '1994-03-13', 'f', 'C', 2, 1900, 1450, '17 Claremont Avenue', '+380665430876', NULL),
+	('Richard Thomas Anderson', '1996-11-28', 'm', 'C', 3, 1999, 2000, '21 Croix Trail', '+380958763144', NULL),
+	('Samantha Beth Hamilton', '1997-10-07', 'f', 'D', 1, 1690, 1900, '6 Huntington Drive', '+380665974439', '7775412'),
+	('Vincent Justin Maxwell', '1996-07-19', 'm', 'E', 4, 1550, 0, '19 Marthas Lane', '+380677885109', NULL),
+	('Rebecca Lauren Williams', '2001-09-09', 'f', 'E', 2, 1520, 0, '16 Peace Way', '+380997859920', '7198923'),
+	('Alexis Tara Larsen', '1990-04-26', 'f', 'B', 4, 2000, 2600, '21 Huntington Drive', '+380665478810', '7145409'),
+	('David Michael Harrison', '1999-08-02', 'm', 'E', 2, 1430, 1150, '16 Crestview Road', NULL, NULL); 
+
+INSERT INTO Competition (comptype, location, compdate, agelimit) VALUES 
+	('International', '24 Patricia Terrace', '2021-11-22', 18),
+	('National', '24 Patricia Terrace', '2022-09-15', 30),
+	('Regional', '17 Lake Street', '2022-06-01', 25),
+	('Local', '4 Pine Avenue', '2022-02-13', 21);
+
+INSERT INTO Participation (competition, sportsman, result, place) VALUES 
+	(1, 1, 300, 1),
+	(1, 2, 100, NULL),
+	(1, 3, 270, 2),
+	(1, 4, 50, 10),
+	(2, 5, 200, 2),
+	(2, 6, 330, 1),
+	(2, 7, 100, 3),
+	(3, 8, 150, 2),
+	(3, 9, 200, 1),
+	(4, 10, 70, NULL);
+
+SELECT * FROM Trainers;
+SELECT * FROM Sportsmen;
+SELECT * FROM Competition;
+SELECT * FROM Participation;
+
+-- examples of using the ALTER TABLE command
+ALTER TABLE Competition ADD starttime DATETIME;
+SELECT * FROM Competition;
+DESCRIBE Competition;
+
+ALTER TABLE Competition MODIFY COLUMN starttime TIME;
+SELECT * FROM Competition;
+DESCRIBE Competition;
+
+ALTER TABLE Competition DROP COLUMN starttime;
+SELECT * FROM Competition;
+
+ALTER TABLE Competition ADD CONSTRAINT chk_agelimit CHECK(agelimit > 0 AND agelimit < 100);
+SELECT * FROM Competition;
+INSERT INTO Competition (comptype, location, compdate, agelimit) 
+VALUES ('International', '11 Laura Street', '2021-10-30', 180);
+SELECT * FROM Competition;
+
+ALTER TABLE Competition DROP CONSTRAINT chk_agelimit;
+INSERT INTO Competition (comptype, location, compdate, agelimit) 
+VALUES ('International', '11 Laura Street', '2021-10-30', 180);
+SELECT * FROM Competition;
+
+UPDATE Competition SET agelimit = 18 WHERE agelimit > 100;
+SELECT * FROM Competition;
+
+UPDATE Sportsmen SET scholarship = scholarship + 500;
+SELECT * FROM Sportsmen;
+
+SELECT * FROM Participation;
+DELETE FROM Participation WHERE result < 200;
+SELECT * FROM Participation;
+
+--- 
+
+SELECT DISTINCT category FROM Trainers;
+
+SELECT * FROM Competition WHERE agelimit IN (18, 30);
+SELECT * FROM Sportsmen WHERE category NOT IN ('D', 'E');
+SELECT * FROM Trainers WHERE rate BETWEEN 2100 AND 2370;
+
+SELECT * FROM Sportsmen WHERE mobile LIKE '+38095%';
+SELECT * FROM Competition WHERE agelimit LIKE '2_';
+SELECT * FROM Sportsmen WHERE birthDate LIKE '%-05-%';
+SELECT * FROM Sportsmen WHERE birthDate LIKE '%-05-2_';
+
+SELECT * FROM Sportsmen WHERE flName REGEXP 'Hamilton$';
+SELECT * FROM Sportsmen WHERE birthDate REGEXP '^19';
+SELECT * FROM Sportsmen WHERE birthDate REGEXP '^19.{7}2$';
+SELECT * FROM Trainers WHERE rate REGEXP '^2[2-4].0$';	
+SELECT * FROM Competition WHERE location REGEXP '24 Patricia Terrace|17 Lake Street';	
